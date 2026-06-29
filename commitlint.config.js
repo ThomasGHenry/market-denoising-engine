24df634e1d70dbed3a4059448bac63f7e80f19b0
Thomas G Henry <ThomasGHenry@Gmail.com>
2026-06-29 01:20:37 -0300

    fix(ci): add pre-push commit linter and raise header limit to 80

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 80],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'test', 'refactor', 'perf', 'ci', 'build', 'revert']],
   },
